5bd181644f466851780bbf5c9fcade90bf7c0de9
Dzone077 <duarte.f.santos@tecnico.ulisboa.pt>
2023-09-13 11:17:28 +0100

    Formatted

--- lab1.py
+++ lab1.py
@@ -8,12 +8,10 @@ beta = np.array([[0], [0]])
 
 beta = np.linalg.inv(big_x.T.dot(big_x)).dot(big_x.T).dot(y)
 
-SSE = np.sum((np.linalg.norm(y - big_x.dot(beta)))**2)  
+SSE = np.sum((np.linalg.norm(y - big_x.dot(beta))) ** 2)
 print("Beta 0: %.4f" % beta[0][0])
 print("Beta 1: %.2f" % beta[1][0])
 print("Associated Error is: %.4f" % SSE)
 
 print("Predicted value for 25: %.2f" % (beta[0][0] + beta[1][0] * 25))
 print("Predicted value for 34: %.2f" % (beta[0][0] + beta[1][0] * 34))
-
-
