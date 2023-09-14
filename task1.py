994d486f39792d7229324f945d56862411ce8e92
Dzone077 <duarte.f.santos@tecnico.ulisboa.pt>
2023-09-14 11:22:29 +0100

    pain

--- task1.py
+++ task1.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib as plot
 from sklearn.linear_model import Lasso, Ridge
 
 ###########################################################
@@ -15,9 +16,30 @@ small_x = np.load("X_train_regression1.npy")
 y = np.load("y_train_regression1.npy")
 big_x = np.hstack((np.ones((len(small_x), 1)), small_x))
 
+# print(y)
+
 beta = np.linalg.inv(big_x.T.dot(big_x)).dot(big_x.T).dot(y)
 
+beta_t = np.transpose(beta)
+
 SSE = np.sum((np.linalg.norm(y - big_x.dot(beta))) ** 2)
+
+big_x_t = np.transpose(big_x)
+
+print(big_x.shape)
+
+###################################
+
+# beta_t = [beta_0  beta_1  beta_2  beta_3  beta_4  beta_5  beta_6  beta_7  beta_8  beta_9  beta_10]
+# big_x
+
+###################################
+
+expected_y = beta.T.dot(big_x.T)
+print(expected_y)
+
+print("\n")
+
 print("Beta 0: %.4f" % beta[0][0])
 print("Beta 1: %.2f" % beta[1][0])
 print("Associated Error is: %.4f" % SSE)
