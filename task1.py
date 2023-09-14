2bdd04300b1f701b8c77722aa0daf1f8585897c1
Dzone077 <duarte.f.santos@tecnico.ulisboa.pt>
2023-09-14 11:50:08 +0100

    i

--- task1.py
+++ task1.py
@@ -15,18 +15,8 @@ from sklearn.linear_model import Lasso, Ridge
 small_x = np.load("X_train_regression1.npy")
 y = np.load("y_train_regression1.npy")
 big_x = np.hstack((np.ones((len(small_x), 1)), small_x))
-
-# print(y)
-
 beta = np.linalg.inv(big_x.T.dot(big_x)).dot(big_x.T).dot(y)
 SSE = np.sum((np.linalg.norm(y - big_x.dot(beta))) ** 2)
-
-###################################
-
-# beta_t = [beta_0  beta_1  beta_2  beta_3  beta_4  beta_5  beta_6  beta_7  beta_8  beta_9  beta_10]
-# big_x
-
-###################################
 
 expected_y = big_x.dot(beta)
 print("Predictor Results: \n", expected_y)
@@ -47,10 +37,4 @@ small_x_test = np.load("X_test_regression1.npy")
 
 big_x_test = np.hstack((np.ones((len(small_x_test), 1)), small_x_test))
 expected_y_test = big_x_test.dot(beta)
-print("Predictor Results: \n", expected_y_test)
-
-# for i in test_matrix:
-#    for x_test in i:
-#        print("Predicted value for:", x_test)
-#        print(beta[0][0] + beta[1][0] * x_test)
-#        print("\n")
+# print("Predictor Results: \n", expected_y_test)
