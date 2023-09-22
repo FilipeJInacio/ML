d5d8633adb5f2d14de2be31d4c7875071ba9ba4b
FilipeJInacio <filipejinacioXD@gmail.com>
2023-09-22 11:03:23 +0100

    go baby go

--- lab1.py
+++ lab1.py
@@ -1,30 +1,127 @@
 import numpy as np
+from sklearn.linear_model import LinearRegression,Ridge,Lasso
+from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.model_selection import cross_val_score, KFold
+import math
+
 
 
 x = np.load("X_train_regression1.npy")
 y = np.load("y_train_regression1.npy")
-X = np.hstack((np.ones((len(x), 1)), x))
 
-beta = np.linalg.inv(X.T.dot(X)).dot(X.T).dot(y)
+method = 8
 
-SSE = np.sum((np.linalg.norm(y - X.dot(beta))) ** 2)
+if method == 1 or method == 0:
+    X = np.hstack((np.ones((len(x), 1)), x))
+    beta = np.linalg.inv(X.T.dot(X)).dot(X.T).dot(y)
+    prediction_loss = (np.linalg.norm(y - X.dot(beta))) ** 2
+    sse = np.sum(prediction_loss)
+    
+    print("=============Least Squares=============")
+    print("Error: %.4f" % sse)
+    print(f"Beta: {beta}")
+    
+if method == 2 or method == 0:
+    # Remove mean
+    row_means = np.mean(x, axis=1)
+    X = x - row_means[:, np.newaxis]
+    Y = y - np.mean(y)
+    
+    beta = np.linalg.inv(X.T.dot(X)).dot(X.T).dot(Y)
+    prediction_loss = (np.linalg.norm(y - X.dot(beta))) ** 2
+    sse = np.sum(prediction_loss)
 
-print(SSE)
+    print("=============Least Squares no mean=============")
+    print("Error: %.4f" % sse)
+    print(f"Beta: {beta}")
 
-"""
-small_x = np.array([[24], [30], [36]])
-y = np.array([[13], [14], [16]])
-big_x = np.hstack((np.ones((3, 1)), small_x))
+if method == 3 or method == 0:
+    linear_regression = LinearRegression()
+    linear_regression.fit(x, y)
+    
+    # Make predictions using the model
+    y_pred = linear_regression.predict(x)
 
-beta = np.array([[0], [0]])
+    sse = mean_squared_error(y, y_pred) * len(y)
+    r_squared = r2_score(y, y_pred)
+    
+    print("=============Linear Regression=============")
+    print("Error: %.4f" % sse)
+    print("R2: %.4f" % r_squared)
+    print(f"Beta: {linear_regression.coef_}")
 
-beta = np.linalg.inv(big_x.T.dot(big_x)).dot(big_x.T).dot(y)
+if method == 4 or method == 0:
+    
+    values = []
+    
+    # Alphas values
+    start = 0.1
+    end = 10000
+    step = 0.1
+    alphas = np.arange(start, end + step, step)
+    
+    for alpha in alphas:
+        ridge = Ridge(alpha=alpha)
+        ridge.fit(x, y)
+        values.append([alpha,ridge.score(x, y)])
+    
+    with open("ridge.txt", "w") as file:
+        for alpha,value in values:
+            file.write(f"{alpha}\t{value}\n")
 
-SSE = np.sum((np.linalg.norm(y - big_x.dot(beta))) ** 2)
-print("Beta 0: %.4f" % beta[0][0])
-print("Beta 1: %.2f" % beta[1][0])
-print("Associated Error is: %.4f" % SSE)
+if method == 5 or method == 0:
+    
+    values = []
+    
+    # Alphas values
+    start = 0.1
+    end = 10000
+    step = 0.1
+    alphas = np.arange(start, end + step, step)
+    
+    for alpha in alphas:
+        lasso = Lasso(alpha=alpha)
+        lasso.fit(x, y)
+        values.append([alpha,ridge.score(x, y)])
+        
+    with open("lasso.txt", "w") as file:
+        for alpha,value in values:
+            file.write(f"{alpha}\t{value}\n")
 
-print("Predicted value for 25: %.2f" % (beta[0][0] + beta[1][0] * 25))
-print("Predicted value for 34: %.2f" % (beta[0][0] + beta[1][0] * 34))
-"""
+if method == 6 or method == 0:
+    regression_model = LinearRegression()
+    scores = cross_val_score(regression_model, x, y, cv=6, scoring='r2')
+    
+    print("=============Cross Validation=============")
+    print(f"R2: {scores}")
+    
+if method == 7 or method == 0:
+    regression_model = LinearRegression()
+    scores = cross_val_score(regression_model, x, y, cv=2, scoring='r2')
+    
+    print("=============Cross Validation=============")
+    print(f"R2: {scores}")
+    
+if method == 8 or method == 0:
+    
+    values = []
+    
+    kf = KFold(n_splits=15)
+
+    # Iterate through the folds and split the data
+    for train_indices, validation_indices in kf.split(x):
+        X_train, X_validation = x[train_indices], x[validation_indices]
+        y_train, y_validation = y[train_indices], y[validation_indices]
+        
+        linear_regression = LinearRegression()
+        linear_regression.fit(X_train, y_train)
+        y_pred = linear_regression.predict(x)
+
+        sse = mean_squared_error(y, y_pred) * len(y)
+        r_squared = r2_score(y, y_pred)
+        values.append(r_squared)
+    
+    print("=============Cross Validation=============")
+    print(f"R2: {values}")
+    print(sum(values) / len(values))
+        
---
+++ test.m
@@ -0,0 +1,10 @@
+
+figure(1)
+
+
+
+plot(ridge.VarName1,ridge.VarName2, linewidth = 2);
+hold on
+
+plot(lasso.VarName1,lasso.VarName2, linewidth = 2);
+
