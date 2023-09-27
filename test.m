54737919b0f0cb3f1882d12bf7f82cd21a705d3f
FilipeJInacio <filipejinacioXD@gmail.com>
2023-09-27 13:50:48 +0100

    wgsgsgsdgsdgsgd sdg sdgsd gs dg sdgsdg sdgsdgs dgs dgs dgs dgsd gssdg sdgsdgsd gssdgsd gssdgsdg

--- test.m
+++ test.m
@@ -1,27 +1,7 @@
 
 figure(1)
 
-
-
-plot(ridgecrossV.VarName1,ridgecrossV.VarName3, linewidth = 2);
-hold on
-title("R^2")
-
-figure(2)
-plot(ridgecrossV.VarName1,ridgecrossV.VarName4, linewidth = 2);
-
+plot(elasticx.VarName1,elasticy.VarName1, linewidth = 2);
+xlim([0,0.2])
 title("SSE")
 
-figure(3)
-
-
-
-plot(ridgecrossV2old.VarName1,ridgecrossV2old.VarName3, linewidth = 2);
-hold on
-title("R^2")
-xlim([0,10])
-
-figure(4)
-plot(ridgecrossV2old.VarName1,ridgecrossV2old.VarName4, linewidth = 2);
-xlim([0,10])
-title("SSE")
